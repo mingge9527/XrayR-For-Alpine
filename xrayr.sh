e01777f9c08ff665a87fc2cf4d993df14c90e730
mingge9527 <130334101+mingge9527@users.noreply.github.com>
2023-09-11 11:49:23 +0800

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -2,6 +2,7 @@
 
 # 发送欢迎消息
 echo "XrayR一键安装脚本"
+echo "仅支持Alpine amd64架构"
 echo "安装目录位于 /etc/XrayR"
 echo "进程保活采用OpenRC方式"
 echo "请选择:"
@@ -10,7 +11,8 @@ echo "2:卸载XrayR"
 echo "3:启动XrayR"
 echo "4:停止XrayR"
 echo "5:重启XrayR"
-echo "6:退出脚本"
+echo "6:快速配置XrayR（实验性）"
+echo "7:退出脚本"
 
 # 检查XrayR是否安装
 if [ -d "/etc/XrayR" ]; then
@@ -65,6 +67,7 @@ install_xrayr() {
         exit 1
     fi
     
+
     # 检查系统架构是否为amd64
     if [ "$(uname -m)" != "x86_64" ]; then
         echo "该脚本暂只支持amd64架构"
@@ -168,6 +171,97 @@ case "$option" in
         exit 0
         ;;
     "6")
+        if [ ! -d "/etc/XrayR" ]; then
+            echo "你似乎并未安装XrayR"
+            exit 1
+        fi
+        
+        if [ ! -f "/etc/XrayR/config.yml" ]; then
+            echo "XrayR配置文件不存在，你还没有安装XrayR或配置文件丢失！"
+            exit 1
+        fi
+        
+        echo "请注意，此为实验性功能，若出问题请手动修改配置文件"
+        echo ""
+        echo "面板类型默认设置为V2board，自动进行下一步"
+        echo ""
+        
+        # 设定机场地址
+        echo "设定机场地址"
+        echo ""
+        read -p "请输入你的机场地址:" apihost
+        [ -z "${apihost}" ]
+        echo "---------------------------"
+        echo "您设定的机场网址为 ${apihost}"
+        echo "---------------------------"
+        echo ""
+        
+        
+        # 设定API Key
+        echo "设定与面板对接的API Key"
+        echo ""
+        read -p "请输入API Key:" apikey
+        [ -z "${apikey}" ]
+        echo "---------------------------"
+        echo "您设定的API Key为 ${apikey}"
+        echo "---------------------------"
+        echo ""
+    
+        # 设置节点序号
+        echo "设定节点序号"
+        echo ""
+        read -p "请输入V2Board中的节点序号:" node_id
+        [ -z "${node_id}" ]
+        echo "---------------------------"
+        echo "您设定的节点序号为 ${node_id}"
+        echo "---------------------------"
+        echo ""
+
+        # 选择协议
+        echo "选择节点类型(默认Shadowsocks)"
+        echo ""
+        read -p "请输入你使用的协议(V2ray, Shadowsocks, Trojan):" node_type
+        [ -z "${node_type}" ]
+    
+        # 如果不输入默认为Shadowsocks
+        if [ ! $node_type ]; then 
+        node_type="Shadowsocks"
+        fi
+
+        echo "---------------------------"
+        echo "您选择的协议为 ${node_type}"
+        echo "---------------------------"
+        echo ""
+
+        # 输入域名（Trojan证书申请）
+        echo "输入你的域名（Trojan证书申请）"
+        echo ""
+        read -p "请输入你的域名(node.v2board.com)如无需证书请直接回车:" node_domain
+        [ -z "${node_domain}" ]
+
+        # 如果不输入默认为node1.v2board.com
+        if [ ! $node_domain ]; then 
+        node_domain="node.v2board.com"
+        fi
+
+        # 写入配置文件
+        echo "正在尝试写入配置文件..."
+        sed -i "s/PanelType:.*/PanelType: \"V2board\"/g" /etc/XrayR/config.yml
+        sed -i "s,ApiHost:.*,ApiHost: \"${apihost}\",g" /etc/XrayR/config.yml
+        sed -i "s/ApiKey:.*/ApiKey: ${apikey}/g" /etc/XrayR/config.yml
+        sed -i "s/NodeID:.*/NodeID: ${node_id}/g" /etc/XrayR/config.yml
+        sed -i "s/NodeType:.*/NodeType: ${node_type}/g" /etc/XrayR/config.yml
+        sed -i "s/CertDomain:.*/CertDomain: \"${node_domain}\"/g" /etc/XrayR/config.yml
+        echo ""
+        echo "写入完成，正在尝试重启XrayR服务..."
+        echo
+        rc-service XrayR restart
+        
+        echo "XrayR服务已经完成重启，请愉快地享用！"
+        echo
+        exit 0
+        ;;
+    "7")
         echo "已退出脚本"
         exit 0
         ;;
